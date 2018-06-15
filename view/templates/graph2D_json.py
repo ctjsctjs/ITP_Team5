6d55cd4c9cd440facb0b0bcd859a72eebe3709df
Jiro0451 <jchan0451@gmail.com>
2018-06-15 20:47:02 +0800

    Scatter plot fix
    Reincluded scatter graphs in graph2D_json

--- view/templates/graph2D_json.py
+++ view/templates/graph2D_json.py
@@ -1,5 +1,8 @@
+import plotly.graph_objs as go
+
+
 # Placeholder method TODO: Remove once deemed unnecessary
-def generate_graph2D(dataframe, x_axis, y_axis):
+def generate_graph2D_bar(dataframe, x_axis, y_axis):
     graph2D = {
         'data': [{
             'x': dataframe[x_axis],
@@ -19,5 +22,29 @@ def generate_graph2D(dataframe, x_axis, y_axis):
     return graph2D
 
 
-def generate_graph2D_actual(dataframe, x_axis, y_axis, dummy):
-    return
+def generate_graph2D(dataframe, x_axis, y_axis):
+    figure = {
+        'data': [
+            go.Scatter(
+                x=dataframe[x_axis],
+                y=dataframe[y_axis],
+                text='THIS IS A GRAPH',
+                mode='markers',
+                opacity=0.7,
+                marker={
+                    'size': 15,
+                    'line': {'width': 0.5, 'color': 'white'}
+                },
+                name='THIS IS NAME'
+            )
+        ],
+        'layout': go.Layout(
+            xaxis={'title': x_axis},
+            yaxis={'title': y_axis},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            legend={'x': 0, 'y': 0},
+            hovermode='closest'
+        )
+    }
+
+    return figure
