d31b7f4d7da0c1ad44449711dc31df598d2e318c
ctjsctjs <ctjsctjs@gmail.com>
2018-07-21 21:31:01 +0800

    Code Cleanup

--- controller/page_controllers/GenerateGraph.py
+++ controller/page_controllers/GenerateGraph.py
@@ -742,21 +742,21 @@ def update_filer(value, mode):
 
 #
 # callback to hide generate graph button and show update button after graph generated
-@app.callback(
-    Output('gen-button-1', 'style'),
-    [Input('gen-button-1', 'n_clicks')])
-def update_style(value):
-    if value > 0:
-        return {'display': 'none'}
+# @app.callback(
+#     Output('gen-button-1', 'style'),
+#     [Input('gen-button-1', 'n_clicks')])
+# def update_style(value):
+#     if value > 0:
+#         return {'display': 'none'}
 
 
 # callback to show submit graph button and show update button after graph generated
-@app.callback(
-    Output('gen-filter-submit', 'style'),
-    [Input('gen-button-1', 'n_clicks')])
-def update_style(value):
-    if value > 0:
-        return {'display': 'block'}
+# @app.callback(
+#     Output('gen-filter-submit', 'style'),
+#     [Input('gen-button-1', 'n_clicks')])
+# def update_style(value):
+#     if value > 0:
+#         return {'display': 'block'}
 
 
 # callback for retriving inputs
